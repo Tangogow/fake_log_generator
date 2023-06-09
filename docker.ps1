31251a0efaca823635cc3677b0e65ff7938d5561
Tangogow <tangogow@gmail.com>
2023-06-09 08:53:40 -0700

    adding exec and gen action

--- docker.ps1
+++ docker.ps1
@@ -7,35 +7,52 @@ $size = $Args[5]
 $ip = "172.22.39."
 $name = "flg"
 $image = "flg"
+$eventApp = "MyApp" # If new App, it will create it
 $volume = "C:\ProgramData\Docker\volumes\logs\_data"
 
 Function usage {
     Write-Output "
-Usage: .\docker.ps1 <start|stop|restart|create|rm|recreate|run|exec|logs> <rangemin> <rangemax> [<number_of_logs> <logs_per_seconde> <size_of_logs>]
+Usage: .\docker.ps1 <start|stop|restart|create|rm|recreate|run|exec|gen|logs> <rangemin> <rangemax> [<number_of_logs> <logs_per_seconde> <size_of_logs>]
 Example: 
     .\docker.ps1 run 1 20                   Create and start 20 containers
-    .\docker.ps1 exec 1 20 10000 100 100    Generate 10000 logs of 100 bytes at the rate of 100 logs/s 
-    .\docker.ps1 logs 1 20                  Wait for the logs to come and report
-    .\docker.ps1 rm 1 20                    Kill and remove containers"
+    .\docker.ps1 gen 1 20 10000 100 100     Generate 10000 logs of 100 bytes at the rate of 100 logs/s 
+    .\docker.ps1 logs 1 20                  Wait for the logs to come and report.
+    
+Other command:
+    .\docker.ps1 rm 1 20                    Kill and remove containers
+    .\docker.ps1 restart 1 10               Restart the container from 1 to 10
+    .\docker.ps1 exec 5 10 ""echo hello""   Injecting command into container 5 to 10 (background mode)"
+    exit 1
 }
 
-$actionArray = @("start", "stop", "restart", "create", "rm", "recreate", "run", "exec", "logs")
+$actionArray = @("start", "stop", "restart", "create", "rm", "recreate", "run", "exec", "gen", "logs")
 
 If ($actionArray -notcontains $action) {
     Write-Error "Wrong action command:" $action
     usage
 }
 
+If ($action -eq "exec") {
+    $execCommand = $Args[3]
+    $nblogs = ""
+}
+
 If ($rangemax -eq $null) {
     $rangemax = $rangemin
 }
-If ($rangemin -lt 1 -and $rangemin -match "^\d+$" -or $rangemax -lt 1 -and $rangemax -match "^\d+$") {
+If ($rangemin -lt 1 -or $rangemax -lt 1) {
     Write-Error "Wrong range. Need postive integers"
     usage
 }
 ElseIf ($rangemin -gt $rangemax) {
     Write-Error "Wrong range. Rangemax should be higher than rangemin"
     usage
+}
+if ($action -eq "gen") {
+    if ($nblogs -lt 1 -and $nblogs -match "^\d+$" -or $lps -lt 1 -and $lps -match "^\d+$"  -or $size -lt 1 -and $size -match "^\d+$") {
+        Write-Error "Wrong number of logs and/or logs per second and/or size. Need postive integers"
+        usage
+    }
 }
 
 $logsGenerated = 0
@@ -74,7 +91,7 @@ function formatSize($TotalSize) {
     }
 }
 
-If ($action -eq "exec") {
+If ($action -eq "gen") {
     Remove-Item -Path "$volume\*" -Force -Recurse -ErrorAction SilentlyContinue
 }
 ElseIf ($action -eq "logs") {
@@ -86,9 +103,8 @@ ElseIf ($action -eq "logs") {
     $files = Get-ChildItem -Path $volume -File
     foreach ($file in $files) {
         $content = Get-Content $file.FullName
-
         foreach ($line in $content) {
-            $values = $line.Split(',')
+            $values = $line.Split(',') # parsing csv into array
             if ($values.Length -eq 4) {
                 $logsGenerated += [int]$values[0]
                 $realLogsPerSecond += [int]$values[1]
@@ -111,7 +127,6 @@ Real Duration      $Duration"
 }
 
 For ($i = $rangemin; $i -le $rangemax; $i++) {
-    Write-Output "$action container $i"
     If ($action -eq "start") {
         docker start $name$i
     }
@@ -122,26 +137,36 @@ For ($i = $rangemin; $i -le $rangemax; $i++) {
         docker restart $name$i
     }
     ElseIf ($action -eq "rm") {
-        docker kill $name$i
-        docker rm $name$i
+        docker kill $name$i > $null
+        docker rm $name$i > $null
+        Write-Output "Container $name$i killed"
     }
     ElseIf ($action -eq "create") {
+        echo $ip$i
         docker create --name $name$i --ip $ip$i -v logs:"c:\logs" -e NAME=$name$i -ti $image
+        Write-Output "Container $name$i created"
     }
     ElseIf ($action -eq "recreate") {
-        docker kill $name$i
-        docker rm $name$i
+        docker kill $name$i > $null
+        docker rm $name$i > $null
         docker run --name $name$i --ip $ip$i -v logs:"c:\logs" -e NAME=$name$i -tid $image
+        Write-Output "Container $name$i recreated"
     }
     ElseIf ($action -eq "run") {
         docker kill $name$i > $null
         docker rm $name$i > $null
         docker run --name $name$i --ip $ip$i -v logs:"c:\logs" -e NAME=$name$i -tid $image
+        Write-Output "Container $name$i created and running"
     }
     ElseIf ($action -eq "exec") {
-        docker exec -d $name$i powershell C:\genlog.ps1 MyApp $nblogs $lps $size
+        docker exec -d $name$i powershell $execCommand
+        Write-Output "Command $execCommand injected in container $name$i"
+    }
+    ElseIf ($action -eq "gen") {
+        docker exec -d $name$i powershell C:\genlog.ps1 $eventApp $nblogs $lps $size
+        Write-Output "Generating logs in container $name$i "
     }
 }
-if ($action -eq "run") {
+if ($action -eq "run" -or $action -eq "create") {
     docker ps
 }
